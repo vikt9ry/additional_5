47918dd73083eca42f8c7efe5163da553ae6e693
vikt9ry <timoshenko_vika@mail.ru>
2018-03-02 15:22:34 +0300

    additional_5 : solution_vikt9ry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // solution
+
+  var depth = 0;
+  
+  for(var i in str){   
+
+      if(str[i] == '('){
+          
+          depth ++;
+
+      } else if(str[i] == ')') {
+          
+          depth --;
+      }  
+     
+      if(str[i] == '[' ){
+          
+        depth ++;
+
+    } else if(str[i] == ']') {
+        
+        depth --;
+    }   
+
+    if(str[i] == '{' ){
+          
+      depth ++;
+      
+  } else if(str[i] == '}') {
+      
+      depth --;
+  }  
+ 
+      if (depth < 0) return false;
+  }
+  
+  if(depth > 0) return false;
+  
+  return true;
 }
